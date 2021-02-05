e9badc43c4bca03928806cb43c1fc403b7cbdb92
Nathaniel Wheeler <nathan@mailftp.com>
2021-02-05 14:18:24 -0700

    restructure to better match standards

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,15 +7,29 @@ import (
 
 // Routes sets up all routes in the server.
 func (s *Server) Routes() {
-	r := s.Router
+  r := s.Router
+  var err error
 
-	assetsDir := "assets"
-	_, err := os.Stat(assetsDir)
-	if err != nil {
-		s.Logger.Printf("%s\n", err)
+  // Setup Fileserver
+  // fsDir is a simple struct that is only used to make fileserver setup easier.
+	type fsDir struct {
+		prefix string
+		dir    string
   }
-  // Serves files under http://localhost:1729/assets/<filename>
-  r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(assetsDir))))
-  
-  r.HandleFunc("/", s.HandleHome())
+  // NOTE Append new fileservers here
+  var fsDirs []fsDir
+  fsDirs = append(fsDirs, fsDir{prefix: "/assets/", dir: "assets"})
+  fsDirs = append(fsDirs, fsDir{prefix: "/app/", dir: "web/app"})
+  fsDirs = append(fsDirs, fsDir{prefix: "/static/", dir: "web/static"})
+  for _, v := range fsDirs {
+    // Check if directory exists
+    _, err = os.Stat(v.dir)
+    if err != nil {
+      s.Logger.Printf("%s\n", err)
+    }
+    // Add to router
+    r.PathPrefix(v.prefix).Handler(http.StripPrefix(v.prefix, http.FileServer(http.Dir(v.dir))))
+  }
+
+	r.HandleFunc("/", s.HandleHome())
 }
--- web/main.tpl
+++ web/main.tpl
@@ -5,11 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
-  <script src="/assets/wasm_exec.js"></script>
+  <script src="/static/wasm_exec.js"></script>
   <script>
     const go = new Go();
     WebAssembly
-      .instantiateStreaming(fetch("/assets/json.wasm"), go.importObject)
+      .instantiateStreaming(fetch("/app/json.wasm"), go.importObject)
       .then((result) => {
         go.run(result.instance);
       });
