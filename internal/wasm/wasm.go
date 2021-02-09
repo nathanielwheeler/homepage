79ad0eb1bc0f4f1d555737133ccca3f0f6f4530e
Nathaniel Wheeler <nathan@mailftp.com>
2021-02-09 15:06:14 -0700

    frontend funcmap

--- internal/wasm/wasm.go
+++ internal/wasm/wasm.go
@@ -1,9 +1,34 @@
 package wasm
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js" // ignore, build handles
+)
 
+// Run will start frontend processes, returning an error if there is any.
 func Run() error {
 	var err error
 	fmt.Println("Go Web Assembly")
+
+	SetupFuncMap()
+
+	// Wait for commands on channel
+	<-make(chan bool)
 	return err
+}
+
+// SetupFuncMap will make listed go functions globally accessible to the frontend.
+func SetupFuncMap() {
+	funcMap := make(map[string]js.Func)
+
+	funcMap["echo"] = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		for i, v := range args {
+			fmt.Printf("%d: %v\n", i, v)
+		}
+		return ""
+	})
+
+	for k, v := range funcMap {
+		js.Global().Set(k, v)
+	}
 }
